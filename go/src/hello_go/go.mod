4fdc2ba47c754179cc39af9f7894f4d45a992cf5
Sonia Santiago <imeonsonia@gmail.com>
2024-09-06 23:28:10 -0600

    Reestructura de archivos

--- go/src/hello_go/go.mod
+++ go/src/hello_go/go.mod
@@ -1,3 +1,3 @@
 module hello_go/hello
 
-go 1.23
+go 1.22.6
--- go/src/hello_go/hello.go
+++ go/src/hello_go/hello.go
@@ -3,8 +3,6 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Funcion de Git")
-    fmt.Println("Funcion de Git- status=muestra los cambios o el estatus de tus archivos ")
-    fmt.Println("Funcion de Git- clone= clona un repositorio")
-    fmt.Println("Funcion de Git dff =Muestra los cambios por linea en un archivo")
+    fmt.Println("¡Hola Mundo!")
+
 }
