5bbac8330e0fd9ebe6f6d02a138a2c4bc9177a30
enrique <enrique@MacbookAir-M3-Henyi.local>
2024-11-08 18:03:19 -0600

    add movielens julia update

--- julia/src/student_submissions/movielens/gomez_enrique/README.md
+++ julia/src/student_submissions/movielens/gomez_enrique/README.md
@@ -8,6 +8,4 @@ julia movielens.jl "ratings.csv"
 
 # Missing funcionality
 
-So far I've implemented the "big file" split.
-
-It is only a matter of reading and processing the splits to get each genre rating average.
+So far I've implemented the "big file" split and summing ratings with DataFrames.jl.
--- julia/src/student_submissions/movielens/gomez_enrique/movielens.jl
+++ julia/src/student_submissions/movielens/gomez_enrique/movielens.jl
@@ -1,4 +1,8 @@
-function split!(filename::String, nbytes::Int)
+using DataFrames
+using Glob
+using CSV
+
+function splitFile(filename::String, nbytes::Int)
     # Open file to split
     file = open(filename, "r")
 
@@ -34,4 +38,32 @@ function split!(filename::String, nbytes::Int)
     close(file)
 end
 
-split!(ARGS[1], 50*1024*1024)
+function average(dirPath::String)
+    dfMovies = DataFrame(CSV.File("../data/movies.csv"))
+    select!(dfMovies, [:movieId, :genres])
+
+    files = filter!(x->contains(x, r"^tmp.*\.csv$"), readdir(dirPath))
+    genres = Dict()
+    for file in files
+        open(file, "r") do f
+            dfRatings = DataFrame(CSV.File("../data/ratings.csv"))
+            select!(dfRatings, [:movieId, :rating])
+
+            dfMerge = groupby(innerjoin(dfMovies, dfRatings, on = :movieId), :genres)
+            dfSum = combine(dfMerge, :rating => sum)
+
+            for row in eachrow(dfSum)
+                for key in split(row[1], "|")
+                    if !haskey(genres, key)
+                        genres[key] = 0
+                    end
+                    genres[key] += row[2]
+                end
+            end
+        end
+    end
+    println(genres)
+end
+
+splitFile(ARGS[1], parse(Int64, ARGS[2])*1024*1024)
+average(ARGS[3])
